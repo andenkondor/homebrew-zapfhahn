643783517bb28178ebbe24179c79f54d50169991
Timo <t.rolfsmeier@live.de>
2025-04-30 10:05:17 +0200

    grec 0.1.3

--- Formula/grec.rb
+++ Formula/grec.rb
@@ -1,7 +1,7 @@
 class Grec < Formula
   desc "[g]it [rec]ent - a cli for showing the latest checked out branches in a repository. Additionally provides a shortcut for checking out"
-  version "tbd"
-  url "tbd"
-  sha256 "tbd"
+  version "0.1.3"
+  url "https://github.com/andenkondor/grec/releases/download/v0.1.3/recent-macos-amd64"
+  sha256 "93e4fca25767750cd658723dbc6910f8015b7d91449497da7700441adad4c2e9"
 end
 
