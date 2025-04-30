acb2b8f2e8701e00cce43f46246f90ecb9a4ef78
Timo Rolfsmeier <timo.rolfsmeier@device-insight.com>
2025-04-30 11:44:20 +0200

    fix: missing installation

--- Formula/grec.rb
+++ Formula/grec.rb
@@ -3,5 +3,9 @@ class Grec < Formula
   version "0.1.6"
   url "https://github.com/andenkondor/grec/releases/download/v0.1.6/grec-macos-amd64.tar.gz"
   sha256 "a6bc22aa44f164dd38e085e84646c10226b207abfc24bca8d5a28117ba18db90"
+
+  def
+    bin.install "grec"
+  end
 end
 
