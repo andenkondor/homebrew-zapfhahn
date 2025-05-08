9b6ee586f87a202d93ada2d874e69a6fe62f569e
Timo Rolfsmeier <timo.rolfsmeier@device-insight.com>
2025-05-08 18:17:59 +0200

    chore: add dependencies for kommit

--- Formula/kommit.rb
+++ Formula/kommit.rb
@@ -4,6 +4,12 @@ class Kommit < Formula
   url "https://github.com/andenkondor/kommit/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "50547e00017b4e768af637d4a3b591f692d8d2bd326fec178d0b85fcd9be7b17"
 
+  depends_on "git"
+  depends_on "zsh"
+  depends_on "fzf"
+  depends_on "gnu-sed"
+  depends_on "neovim"
+
   def install
     bin.install "kommit.zsh" => "kommit"
   end
