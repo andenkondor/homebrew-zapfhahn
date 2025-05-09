27622dd96994c65d1cd15b4af581d38d01bb443d
T <t.rolfsmeier@live.de>
2025-05-09 17:08:47 +0200

    kommit 0.1.4

--- Formula/kommit.rb
+++ Formula/kommit.rb
@@ -1,8 +1,8 @@
 class Kommit < Formula
   desc "Easily write conventional commit messages"
-  version "0.1.1"
-  url "https://github.com/andenkondor/kommit/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "a75fb0bf0c699fe39edce548279685a4fe24f5c384281cd28c50dd9b1cce5433"
+  version "0.1.4"
+  url "https://github.com/andenkondor/kommit/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "fde90bf2de6048955efb4ae033fe02de897132663769a7018967e32cfc75ced0"
 
   depends_on "git"
   depends_on "zsh"
