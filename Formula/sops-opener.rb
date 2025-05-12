c68077a58d1367c2bb1aca1c4a1b843a0319539a
T <t.rolfsmeier@live.de>
2025-05-12 16:22:42 +0200

    sops-opener 0.1.0

--- Formula/sops-opener.rb
+++ Formula/sops-opener.rb
@@ -1,8 +1,8 @@
 class SopsOpener < Formula
   desc "Open sops and normal files with a single command"
-  version "tbd"
-  url "tbd"
-  sha256 "tbd"
+  version "0.1.0"
+  url "https://github.com/andenkondor/sops-opener/releases/download/v0.1.0/sops-opener.tar.gz"
+  sha256 "010ec76215110a30b9fc1329f50c9941d4ffc15618290ba7a8b175ce0ae8a239"
 
   depends_on "zsh"
   depends_on "sops"
