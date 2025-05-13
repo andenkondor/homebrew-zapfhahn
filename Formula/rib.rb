77fa12947d2f3d1adea2714031f37e3c14f3cbb5
T <t.rolfsmeier@live.de>
2025-05-13 14:48:24 +0200

    rib 0.1.0

--- Formula/rib.rb
+++ Formula/rib.rb
@@ -1,8 +1,8 @@
 class Rib < Formula
   desc "[r]esource [i]n [b]rowser - find your Azure resources with fzf"
-  version "tbd"
-  url "tbd"
-  sha256 "tbd"
+  version "0.1.0"
+  url "https://github.com/andenkondor/rib/releases/download/v0.1.0/rib.tar.gz"
+  sha256 "91d6bfe29f60d1d47a895c206fd91c761f3f7e2065656c35fb1efca8754a71b1"
 
   depends_on "fzf"
   depends_on "azure-cli"
