81d34eb5eda7a9edef7ccd79d86c683f9cb8c614
Timo <t.rolfsmeier@live.de>
2025-05-19 13:48:13 +0200

    grec 0.2.0

--- Formula/grec.rb
+++ Formula/grec.rb
@@ -1,8 +1,8 @@
 class Grec < Formula
   desc "[g]it [rec]ent - a cli for showing the latest checked out branches in a repository. Additionally provides a shortcut for checking out"
-  version "0.1.7"
-  url "https://github.com/andenkondor/grec/releases/download/v0.1.7/grec-macos-amd64.tar.gz"
-  sha256 "aa0fdd824dad7c799623fabbcf86dcc1aa560fe46d8af1c1ab01c2cac6d49686"
+  version "0.2.0"
+  url "https://github.com/andenkondor/grec/releases/download/v0.2.0/grec-macos-amd64.tar.gz"
+  sha256 "a10ffecb17fee712a5a0020c10dc7a252b3a4b28d2fafbfeddf7caa9497f866a"
 
   def install
     bin.install "grec"
