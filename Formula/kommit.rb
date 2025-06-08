cf66ffe90810faf1ac7ab0bcec5e75c597526aee
Timo Rolfsmeier <timo.rolfsmeier@device-insight.com>
2025-06-08 16:37:05 +0200

    chore: kommit relies on zx

--- Formula/kommit.rb
+++ Formula/kommit.rb
@@ -5,13 +5,11 @@ class Kommit < Formula
   sha256 "f9eeaeb5d657a79939f0bf7348449ba9e5742a0f85af3f92fb75f935213f91c9"
 
   depends_on "git"
-  depends_on "zsh"
   depends_on "fzf"
-  depends_on "gnu-sed"
-  depends_on "neovim"
+  depends_on "zx"
 
   def install
-    bin.install "kommit.zsh" => "kommit"
+    bin.install "kommit.mjs" => "kommit"
   end
 end
 
