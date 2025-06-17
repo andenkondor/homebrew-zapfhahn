39fc4237f545424e7819c0d62b2223c7921a0a28
Timo <t.rolfsmeier@live.de>
2025-06-17 16:23:23 +0200

    encryptify 0.3.0

--- Formula/encryptify.rb
+++ Formula/encryptify.rb
@@ -1,8 +1,8 @@
 class Encryptify < Formula
   desc "Gpg meets fzf"
-  version "0.2.3"
-  url "https://github.com/andenkondor/encryptify/releases/download/v0.2.3/encryptify.tar.gz"
-  sha256 "f5178f836fc345ff90c781a6e83104fad5726f554db9f6193fb98b1f6bc3e9b2"
+  version "0.3.0"
+  url "https://github.com/andenkondor/encryptify/releases/download/v0.3.0/encryptify.tar.gz"
+  sha256 "b3a417feab172c0cde85b1d2e3985936ccbce2b8f91f55aaf85742f99988f6b5"
 
   depends_on "fzf"
   depends_on "zx"
