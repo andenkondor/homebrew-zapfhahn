f9d22ca7e7b3e006f32e4ef945ff5babd9fcf0b7
Timo <t.rolfsmeier@live.de>
2025-07-24 11:29:55 +0200

    skan 0.2.9

--- Formula/skan.rb
+++ Formula/skan.rb
@@ -1,8 +1,8 @@
 class Skan < Formula
   desc "Fzf wrapper around ripgrep"
-  version "0.2.8"
-  url "https://github.com/andenkondor/skan/releases/download/v0.2.8/skan.tar.gz"
-  sha256 "a8e157d6de0de296ee905206df4659927e7ae66cd8b6c6eefe9fcd8ea7e339ac"
+  version "0.2.9"
+  url "https://github.com/andenkondor/skan/releases/download/v0.2.9/skan.tar.gz"
+  sha256 "1a94d28b19a7b5bce4fc8e76a6c1370bee31b0090f2c032026cd5c5bb2a08481"
 
   depends_on "andenkondor/zapfhahn/sops-opener"
   depends_on "bat"
