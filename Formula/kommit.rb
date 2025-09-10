02d4e2e493c51f547d17934c62c9518dfd1e7c51
Timo <t.rolfsmeier@live.de>
2025-09-10 18:43:52 +0200

    kommit 0.4.0

--- Formula/kommit.rb
+++ Formula/kommit.rb
@@ -1,8 +1,8 @@
 class Kommit < Formula
   desc "Easily write conventional commit messages"
-  version "0.3.7"
-  url "https://github.com/andenkondor/kommit/releases/download/v0.3.7/kommit.tar.gz"
-  sha256 "b08697b20b4415f35a042b2282b2da8310f08d291c39c37436f4209effeffca3"
+  version "0.4.0"
+  url "https://github.com/andenkondor/kommit/releases/download/v0.4.0/kommit.tar.gz"
+  sha256 "f4fabb85c92a9d2e71f4fd862626a749f7feb048f41146e3f920bb59974e078b"
 
   depends_on "git"
   depends_on "fzf"
