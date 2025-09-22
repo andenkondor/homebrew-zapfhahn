dc1150c7761ad7a137a0152f3869f14d404ecfd7
Timo <t.rolfsmeier@live.de>
2025-09-22 21:55:22 +0200

    stemp 0.0.1

--- Formula/stemp.rb
+++ Formula/stemp.rb
@@ -1,8 +1,8 @@
 class Stemp < Formula
   desc "Easily handle timestamps"
-  version "tbd"
-  url "tbd"
-  sha256 "tbd"
+  version "0.0.1"
+  url "https://github.com/andenkondor/stemp/releases/download/v0.0.1/stemp.tar.gz"
+  sha256 "cbc06166a298327a325510c3789fa9dc2106c4a21fca00c69758545a5ad21f26"
 
 
 end
