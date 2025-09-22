f959c377836743e279df4d1da1a9cb85a4dd2163
rolfi <t.rolfsmeier@live.de>
2025-09-22 21:59:54 +0200

    fix: add stemp bin install

--- Formula/stemp.rb
+++ Formula/stemp.rb
@@ -4,6 +4,8 @@ class Stemp < Formula
   url "https://github.com/andenkondor/stemp/releases/download/v0.0.1/stemp.tar.gz"
   sha256 "cbc06166a298327a325510c3789fa9dc2106c4a21fca00c69758545a5ad21f26"
 
-
+  def install
+    bin.install "stemp" => "stemp"
+  end
 end
 
