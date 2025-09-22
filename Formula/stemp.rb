ea7c5b46e820ad3c5e8044913bf4ae45e958da17
rolfi <t.rolfsmeier@live.de>
2025-09-22 22:45:03 +0200

    fix: add bun dependency for stemp

--- Formula/stemp.rb
+++ Formula/stemp.rb
@@ -4,6 +4,8 @@ class Stemp < Formula
   url "https://github.com/andenkondor/stemp/releases/download/v0.0.3/stemp.tar.gz"
   sha256 "1d69588dce4e47f6402338b0cba4d40399d086ad6d3efeeada30dd44c3e7b74f"
 
+  depends_on "oven-sh/bun/bun"
+
   def install
     bin.install "stemp" => "stemp"
   end
