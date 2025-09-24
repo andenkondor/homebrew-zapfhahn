6744b84a9e7352363c2e9731647b1ae11e0a4b47
Timo <t.rolfsmeier@live.de>
2025-09-24 07:37:27 +0200

    stemp 0.0.5

--- Formula/stemp.rb
+++ Formula/stemp.rb
@@ -1,8 +1,8 @@
 class Stemp < Formula
   desc "Easily handle timestamps"
-  version "0.0.3"
-  url "https://github.com/andenkondor/stemp/releases/download/v0.0.3/stemp.tar.gz"
-  sha256 "1d69588dce4e47f6402338b0cba4d40399d086ad6d3efeeada30dd44c3e7b74f"
+  version "0.0.5"
+  url "https://github.com/andenkondor/stemp/releases/download/v0.0.5/stemp.tar.gz"
+  sha256 "e2c5851ff35d5c7aae066a219e0c8184abf34d1db926e15b97c9e526061087d3"
 
   depends_on "oven-sh/bun/bun"
 
