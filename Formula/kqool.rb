707bdc3fe9da4c86c5933d4dd0b5082ccc8ad2ae
Timo <t.rolfsmeier@live.de>
2025-10-23 10:02:26 +0200

    kqool 0.0.4

--- Formula/kqool.rb
+++ Formula/kqool.rb
@@ -1,8 +1,8 @@
 class Kqool < Formula
   desc "Interactively build kql queries"
-  version "tbd"
-  url "tbd"
-  sha256 "tbd"
+  version "0.0.4"
+  url "https://github.com/andenkondor/kqool/releases/download/v0.0.4/kqool.tar.gz"
+  sha256 "346f842bb002ef331e8f953cb3be0c38f0b90b82b04593ae3f453d2e18988ca3"
 
 
   depends_on "fzf"
