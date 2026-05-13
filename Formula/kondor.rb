a5d6b5daedf5089a3a9cf8b3052a95b256bf2e10
Timo <t.rolfsmeier@live.de>
2026-05-13 12:49:28 +0200

    kondor 0.3.1

--- Formula/kondor.rb
+++ Formula/kondor.rb
@@ -1,8 +1,8 @@
 class Kondor < Formula
   desc "Full-text search"
-  version "0.3.0"
-  url "https://github.com/andenkondor/kondor/releases/download/v0.3.0/kondor.tar.gz"
-  sha256 "01fcbd29b2286905bc4d5a758fd7a2de23ba145c4de29c848c30f3434b04e6f5"
+  version "0.3.1"
+  url "https://github.com/andenkondor/kondor/releases/download/v0.3.1/kondor.tar.gz"
+  sha256 "5fce031bb0a48e7fe907ff5a4c2517930ee02664548afb14790e6b27018a740a"
 
   depends_on "oven-sh/bun/bun"
   depends_on "ripgrep"
